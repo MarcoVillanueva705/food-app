d8863b23303031a7a247bde41de43e5d08b385b1
marco villanueva <villanuevamarco98@gmail.com>
2020-03-03 14:32:46 -0700

    data binding test

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="hello">
+    {{foodItem}}
+    <img :src="tacoImage" alt="tacos">
+    {{price}}
+    <button @click="buy">BUY</button>
    
   </div>
 </template>
@@ -7,26 +11,22 @@
 <script>
 export default {
   name: 'HomeComponent',
-  props: {
-    msg: String
+  data() {
+    return {
+      foodItem: 'Taco Lunch',
+      tacoImage:'https://cdn-jaxrestaurant.pressidium.com/wp-content/uploads/2015/05/Corner-Taco-Lunch-1024x791.jpg',
+      price: 3.99,
+    }
+  },
+  methods: {
+    buy(){
+      this.price--
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
